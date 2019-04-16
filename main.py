618486b0f679bdd9cd419e31f8a76a83d0dbc8c3
Chrisjan <chrisjanwust@gmail.com>
2019-04-16 02:39:32 +0200

    initial

---
+++ main.py
@@ -0,0 +1,16 @@
+import obd
+
+obd.logger.setLevel(obd.logging.DEBUG)
+
+# connection = obd.OBD()
+connection = obd.OBD(portstr='\\.\\COM14', baudrate=38400, fast=False)
+
+
+'''
+cmd = obd.commands.RPM # select an OBD command (sensor)
+
+response = connection.query(cmd) # send the command, and parse the response
+
+print(response.value) # returns unit-bearing values thanks to Pint
+print(response.value.to("mph")) # user-friendly unit conversions
+'''
